d3713f27f0d6f5107baa3fcb5c23bfa69cfce815
seong su kim <know.warehouse02@gmail.com>
2025-06-06 11:44:13 +0900

    공통 컴포넌트 추가 / 메타데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,30 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: '빠르게 비즈니스 시작하기',
-  description: '전문적인 랜딩 페이지 제작 서비스를 제공합니다.',
+  title: 'MakeLandingPage',
+  description:
+    '비즈니스 성장을 위한 맞춤형 랜딩 페이지 제작 서비스를 제공합니다.',
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="bg-blob" />
+        <Header />
+        <main className="pt-16">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
