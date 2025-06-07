fc1c68d0affa868ab494faafd1252debdead60de
seong su kim <know.warehouse02@gmail.com>
2025-06-07 12:43:49 +0900

    리뷰틀 주석처리 이후 활성화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from '@/components/sections/HeroSection';
 import { FeaturesSection } from '@/components/sections/FeaturesSection';
 import { PricingSection } from '@/components/sections/PricingSection';
 import { TimeLimitSection } from '@/components/sections/TimeLimitSection';
-import { ReviewsSection } from '@/components/sections/ReviewsSection';
+//import { ReviewsSection } from '@/components/sections/ReviewsSection';
 import { ContactSection } from '@/components/sections/ContactSection';
 
 export default function Home() {
@@ -13,7 +13,7 @@ export default function Home() {
         <FeaturesSection />
         <PricingSection />
         <TimeLimitSection />
-        <ReviewsSection />
+        {/** 리뷰틀만 만들어두고, 리뷰를 받으면 수정 후 활성화 <ReviewsSection /> */}
         <ContactSection />
       </main>
     </>
