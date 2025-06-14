6c076c683d6567104fb29d8cc1fac8e629c275c0
seong su kim <know.warehouse02@gmail.com>
2025-06-14 20:54:46 +0900

    postcss not fix?

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: ['@tailwindcss/postcss'],
 };
 
 export default config;
+// This file is used to configure PostCSS for the Next.js project.
+// 추후 플러그인 지울 수도
