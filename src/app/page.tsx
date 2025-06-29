849322ee293a287e9aaf124497cb13463ac71af5
seong su kim <know.warehouse02@gmail.com>
2025-06-29 19:09:04 +0900

    timelimitsection 주석처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { HeroSection } from '@/components/sections/HeroSection';
-import { FeaturesSection } from '@/components/sections/FeaturesSection';
-import { PricingSection } from '@/components/sections/PricingSection';
-import { TimeLimitSection } from '@/components/sections/TimeLimitSection';
+import { HeroSection } from "@/components/sections/HeroSection";
+import { FeaturesSection } from "@/components/sections/FeaturesSection";
+import { PricingSection } from "@/components/sections/PricingSection";
+//import { TimeLimitSection } from '@/components/sections/TimeLimitSection';
 //import { ReviewsSection } from '@/components/sections/ReviewsSection';
-import { ContactSection } from '@/components/sections/ContactSection';
+import { ContactSection } from "@/components/sections/ContactSection";
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
         <HeroSection />
         <FeaturesSection />
         <PricingSection />
-        <TimeLimitSection />
+        {/** 고객들이 늘어나면 조금 수정하고 다시 오픈. 일단 주석 처리 <TimeLimitSection /> */}
         {/** 리뷰틀만 만들어두고, 리뷰를 받으면 수정 후 활성화 <ReviewsSection /> */}
         <ContactSection />
       </main>
